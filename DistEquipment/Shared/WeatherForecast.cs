47353dd31629de73f0b1a391c98bb7a014eedb41
igor egorov <igor127@bk.ru>
2022-09-03 15:56:58 +0300

    Add project files.

---
+++ DistEquipment/Shared/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace DistEquipment.Shared
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string? Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
