da9379d579252ee5ab244c2886547031bd3ff480
igor egorov <igor127@bk.ru>
2022-10-27 14:44:13 +0300

    add dist and rec bag page

--- DistEquipment/Client/Services/DataProduct.cs
+++ DistEquipment/Client/Services/DataProduct.cs
@@ -9,26 +9,23 @@ namespace DistEquipment.Client.Services
     {
         private readonly HttpClient httpClient;
 
-        public event Action OnChange;
-
-        public List<Product> Products { get; set; } = new List<Product>();
 
         public DataProduct(HttpClient _httpClient)
         {
             httpClient = _httpClient;
         
         }
-        public  async Task LoadProduct(string Url=null)
+        public  async Task<List<Product>> LoadProduct(string Url=null)
         {
             if (Url == null)
             {
-                Products = await httpClient.GetFromJsonAsync<List<Product>>("api/Product");
+                return await httpClient.GetFromJsonAsync<List<Product>>("api/Product");
             }
             else
             {
-                Products = await httpClient.GetFromJsonAsync<List<Product>>($"api/Product/category/{Url}");
+                return  await httpClient.GetFromJsonAsync<List<Product>>($"api/Product/category/{Url}");
             }
-            OnChange.Invoke();
+          
         }
         public async Task<Product> GetProductById(int Id)
         {
--- DistEquipment/Client/Services/IDataProduct.cs
+++ DistEquipment/Client/Services/IDataProduct.cs
@@ -4,9 +4,7 @@ namespace DistEquipment.Client.Services
 {
     public interface IDataProduct
     {
-        event Action OnChange; 
-        List<Product> Products { get; set; }
-        Task LoadProduct(string Url=null);
+        Task<List<Product>> LoadProduct(string Url=null);
 
         Task<List<Product>> GetAllDistillars();
         Task<Product> GetProductById(int Id);
