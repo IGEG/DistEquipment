b3c08985c9dde9523d4c538340187cebd15c7dc3
igor egorov <igor127@bk.ru>
2022-11-01 13:36:46 +0300

    form its done

--- DistEquipment/Shared/FeedBackForm.cs
+++ DistEquipment/Shared/FeedBackForm.cs
@@ -1,12 +1,22 @@
-﻿namespace DistEquipment.Shared
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DistEquipment.Shared
 {
     public class FeedBackForm
     {
+
         public string PageName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Укажите Ваше Имя")]
         public string Name { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Укажите название компании")]
         public string CompanyName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Укажите телефон для связи")]
+        [Phone]
         public string TelephoneNumber { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Укажите адрес электронной почты")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Некорректный адрес")]
         public string Email { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Задайте свой вопрос")]
         public string Message { get; set; } = string.Empty;
    
     }
