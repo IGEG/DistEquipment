25143c9672300852b1e8f0ec678c578d911cb181
igor egorov <igor127@bk.ru>
2022-11-01 11:45:49 +0300

    feedbackform begin

---
+++ DistEquipment/Shared/FeedBackForm.cs
@@ -0,0 +1,13 @@
+﻿namespace DistEquipment.Shared
+{
+    public class FeedBackForm
+    {
+        public string PageName { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+        public string TelephoneNumber { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+   
+    }
+}
